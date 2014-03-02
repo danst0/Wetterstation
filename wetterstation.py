6acaa6a07c91a297cf411bfa4b7f139b1a159f1d
Daniel Dumke <ddumke@gmx.net>
2014-03-02 16:23:48 -0500

    Umsetzung der Empfehlungen von Pylint für wetterstation.py

--- wetterstation.py
+++ wetterstation.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+"""
+Python Wetterstation
 
+Auslesen, aggregieren, speichern und auswerten von Wetterdaten.
+Aktuell können nur vordefinierte Sensoren (I2C und ELV USB-WDE1) ausgelesen werden.
+Die Speicherung erfolgt in einer SQLite Datenbank, die Auswertung/Darstellung per Web-Schnittstelle.
+"""
 import sys
-full_base_path = '/Users/danst/Documents/Archiv/Computer-Elektronik/Wetterstation/'
-sys.path.append(full_base_path)
+FULL_BASE_PATH = \
+    '/Users/danst/Documents/Archiv/Computer-Elektronik/Wetterstation/'
+sys.path.append(FULL_BASE_PATH)
 
-import time, datetime
-from pprint import pprint
+import datetime
+# from pprint import pprint
 
 import random
 import argparse
-import datenbank
-import diagramme
-import cameraremote
+import D3.datenbank
+import D3.diagramme
+import D3.cameraremote
 import serial
-from serial.tools.list_ports import *
+from serial.tools.list_ports import comports
 
 
 
@@ -25,7 +32,10 @@ from serial.tools.list_ports import *
 
 
 class Sensors:
-    daten={'Server': {}, 'Raum1': {}, 'Raum2': {}, u'Außen': {}}
+    """
+    Klasse zum Auslesen der Sensoren über I2C und serieller Schnittstelle.
+    """
+    daten = {'Server': {}, 'Raum1': {}, 'Raum2': {}, u'Außen': {}}
     def __init__(self):
         self.com_port = '/dev/tty.Bluetooth-Modem'
         ports = map(lambda x: x[0], comports())
@@ -38,7 +48,7 @@ class Sensors:
             self.serial.close()
         except:
             print 'Serial: Alles ok, sollte noch gar nicht funktionieren'
-        
+
         if not self.read_radio():
             print('Problem mit dem Empfänger')
             sys.exit(1)
@@ -46,10 +56,13 @@ class Sensors:
             print('Problem mit I2C-Bus')
             sys.exit(1)
 #         pprint(self.daten)
-    
+
     def read_radio(self):
         #string = '$1;1;;;;;;13,0;;;;;;;;58;;;;18,9;39;0,0;2680;0;0'
-        string = '$1;1;;;;;;'+self.random_temp_str()+';;;;;'+self.random_temp_str()+';;;;;;;'+self.random_temp_str()+';;'+self.random_temp_str()+';;;0'
+        string = '$1;1;;;;;;' + self.random_temp_str() + \
+                ';;;;;'+self.random_temp_str() + \
+                ';;;;;;;'+self.random_temp_str() + \
+                ';;'+self.random_temp_str()+';;;0'
 # Check for completeness
         if string[:6] != '$1;1;;' or string[-2:] != ';0':
             return False
@@ -57,12 +70,12 @@ class Sensors:
         fields = string.split(';')
 #         pprint(fields)
 #         print fields[8]
-        self.daten['Raum1']['Temperatur'] = float(fields[7].replace(',','.'))
-        self.daten['Raum1']['Feuchtigkeit'] = float(fields[12].replace(',','.'))
-        self.daten['Raum2']['Temperatur']  = float(fields[19].replace(',','.'))
-        self.daten['Raum2']['Feuchtigkeit'] = float(fields[21].replace(',','.'))
+        self.daten['Raum1']['Temperatur'] = float(fields[7].replace(',', '.'))
+        self.daten['Raum1']['Feuchtigkeit'] = float(fields[12].replace(',', '.'))
+        self.daten['Raum2']['Temperatur'] = float(fields[19].replace(',', '.'))
+        self.daten['Raum2']['Feuchtigkeit'] = float(fields[21].replace(',', '.'))
         return True
-        
+
     def read_i2c(self):
         self.daten['Server']['Temperatur'] = self.random_temp()
         self.daten['Server']['Luftdruck'] = self.random_temp()
@@ -70,20 +83,21 @@ class Sensors:
         self.daten[u'Außen']['Luftdruck'] = self.random_temp()
         self.daten[u'Außen']['Licht'] = self.random_temp()
         return True
-        
+
     def random_temp_str(self):
-        return str(round(((random.random()-0.2/0.5)*0.5)*100, 2)).replace('.',',')
+        return \
+            str(round(((random.random()-0.2/0.5)*0.5)*100, 2)).replace('.', ',')
     def random_temp(self):
         return ((random.random()-0.2/0.5)*0.5)*100
-        
+
     def write_to_file(self):
         filename = 'currentdata.html'
-        f = open(full_base_path + 'html/' + filename, 'r')
-        lines = f.readlines()
-        f.close()
-        f = open(full_base_path + 'html/' + filename, 'w')
+        file_handle = open(FULL_BASE_PATH + 'html/' + filename, 'r')
+        lines = file_handle.readlines()
+        file_handle.close()
+        file_handle = open(FULL_BASE_PATH + 'html/' + filename, 'w')
         current_section = ''
-        previous_line =''
+        previous_line = ''
         first_occurence = True
         for line in lines:
 #             print line,
@@ -110,49 +124,48 @@ class Sensors:
                 first_occurence = True
             elif line.find('Luftdruck') != -1:
                 current_section = 'Luftdruck'
-            elif line.find('Feuchtigkeit') != -1:  
+            elif line.find('Feuchtigkeit') != -1:
                 current_section = 'Feuchtigkeit'
                 first_occurence = True
-            elif line.find('Helligkeit') != -1:  
+            elif line.find('Helligkeit') != -1:
                 current_section = 'Licht'
-            
-            previous_line = line  
-#             print line,  
-            f.write(line)
-        f.close()
-                                
+
+            previous_line = line
+#             print line,
+            file_handle.write(line)
+        file_handle.close()
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Wetterstation')
-    parser.add_argument('--aktualisieren', action='store_true', help='Aktualisiert die Datenbank mit den neuesten Werten')
-    parser.add_argument('--diagramme', action='store_true', help='Generiert die Diagramme zur Anzeige auf der Webseite')
-    parser.add_argument('--erstStart', action='store_true', help='Generiert alle Diagramme neu')
-    parser.add_argument('--kamera', action='store_true', help='Schieße ein neues Bild')
-    parser.add_argument('--kameraIntervall', action='store_true', help='Schieße ein neues Bild, wenn die Zeit gekommen ist')
+    PARSER = argparse.ArgumentParser(description='Wetterstation')
+    PARSER.add_argument('--aktualisieren', action='store_true', help='Aktualisiert die Datenbank mit den neuesten Werten')
+    PARSER.add_argument('--diagramme', action='store_true', help='Generiert die Diagramme zur Anzeige auf der Webseite')
+    PARSER.add_argument('--erstStart', action='store_true', help='Generiert alle Diagramme neu')
+    PARSER.add_argument('--kamera', action='store_true', help='Schieße ein neues Bild')
+    PARSER.add_argument('--kameraIntervall', action='store_true', help='Schieße ein neues Bild, wenn die Zeit gekommen ist')
 
-    args = parser.parse_args()
+    ARGS = PARSER.parse_args()
 #     print(args)
     print "Starte. Uhrzeit: " + str(datetime.datetime.now())
 # Basisobjekte
-    d = datenbank.Database()
-    s = Sensors()
+    D = D3.datenbank.Database()
+    S = Sensors()
 # Update Database?
-    if args.aktualisieren:
-        d.add_all(s.daten)
+    if ARGS.aktualisieren:
+        D.add_all(S.daten)
 #         d.add('Server', 'Temperatur', random.random()*30)
-        d.con.commit()
+        D.con.commit()
 #         print(d.get_latest('Server', 'Temperatur'))
-        s.write_to_file()
-    if args.diagramme:
+        S.write_to_file()
+    if ARGS.diagramme:
 # Generate Graphs
-        g = diagramme.Graphs(d, full_base_path, args.erststart)
-        g.generate_graphs()
-        g.close()
-    if not args.aktualisieren and not args.diagramme:
+        G = D3.diagramme.Graphs(D, FULL_BASE_PATH, ARGS.erstStart)
+        G.generate_graphs()
+        G.close()
+    if not ARGS.aktualisieren and not ARGS.diagramme:
         # Wir wurden ohne Parameter aufgerufen -> CGI Modus
 #         g = diagramme.Graphs()
 #         g.generate_graphs()
         pass
     print("Beende Wetterstation")
-    
+
