e64c4a6119741aa8797891caab7fcbdabf05c43e
Daniel <daniel@dumke.me>
2014-03-26 22:07:53 +0100

    Ergänzung der cronjob-Skripte

---
+++ start_aktualisierung.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ "$(ps -N -p $$ -o comm,pid)" =~ $'\n'"${0##*/}"[[:space:]] ]]; then
+    echo "aready running!"
+    exit 1
+fi
+/home/danst/Wetterstation/wetterstation.py --aktualisieren --kamera
